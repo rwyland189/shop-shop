d2aca48c59148a0cadee72b3fd0d95a493896e0c
Becca Wyland <rwyland@uwalumni.com>
2021-08-03 12:53:10 -0500

    comment out GlobalState.js for the purpose of redux use

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,25 +1,25 @@
-import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers';
+// import React, { createContext, useContext } from "react";
+// import { useProductReducer } from './reducers';
 
-// instantiate global state object
-const StoreContext = createContext();
-const { Provider } = StoreContext;
+// // instantiate global state object
+// const StoreContext = createContext();
+// const { Provider } = StoreContext;
 
-const StoreProvider = ({ value = [], ...props }) => {
-    const [state, dispatch] = useProductReducer({
-      products: [],
-      cart: [],
-      cartOpen: false,
-      categories: [],
-      currentCategory: '',
-    });
-    // use this to confirm it works!
-    console.log(state);
-    return <Provider value={[state, dispatch]} {...props} />;
-};
+// const StoreProvider = ({ value = [], ...props }) => {
+//     const [state, dispatch] = useProductReducer({
+//       products: [],
+//       cart: [],
+//       cartOpen: false,
+//       categories: [],
+//       currentCategory: '',
+//     });
+//     // use this to confirm it works!
+//     console.log(state);
+//     return <Provider value={[state, dispatch]} {...props} />;
+// };
 
-const useStoreContext = () => {
-    return useContext(StoreContext);
-};
+// const useStoreContext = () => {
+//     return useContext(StoreContext);
+// };
 
-export { StoreProvider, useStoreContext };
+// export { StoreProvider, useStoreContext };
