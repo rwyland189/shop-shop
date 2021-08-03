ab6e022acb5d0167bb43663ae80a908435b406e8
Becca Wyland <rwyland@uwalumni.com>
2021-08-03 12:41:37 -0500

    refactor the const client

--- client/src/App.js
+++ client/src/App.js
@@ -21,22 +21,16 @@ import OrderHistory from './pages/OrderHistory';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  request: (operation) => {
+    const token = localStorage.getItem('id_token');
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql',
   cache: new InMemoryCache(),
 });
 
