a7ff79b189700667bf4a9af0fd1449a7359e6e5d
Becca Wyland <rwyland@uwalumni.com>
2021-08-03 12:49:23 -0500

    write const defaultState in the reducers and comment out useReducer from react

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,4 +1,4 @@
-import { useReducer } from 'react';
+// import { useReducer } from 'react';
 
 import {
   UPDATE_PRODUCTS,
@@ -11,6 +11,14 @@ import {
   CLEAR_CART,
   TOGGLE_CART
 } from './actions';
+
+const defaultState = {
+  products: [],
+  cart: [],
+  cartOpen: false,
+  categories: [],
+  currentCategory: ''
+}
   
   export const reducer = (state, action) => {
     switch (action.type) {
